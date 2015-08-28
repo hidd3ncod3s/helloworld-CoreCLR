11e6eacbc90803a2dfacd7be64a49a909cf4f2d2
hidd3ncod3s <hidd3ncod3s@gmail.com>
2015-08-28 08:13:30 -0700

    Removed one more dependency

--- build.bat
+++ build.bat
@@ -10,9 +10,9 @@ cd \src\coreclr
 build clean skiptestbuild
 copy /Y bin\Product\Windows_NT.x64.debug\coreclr.dll \coreclr-demo\runtime
 copy /Y bin\Product\Windows_NT.x64.debug\mscorlib.dll \coreclr-demo\runtime
+copy /Y bin\Product\Windows_NT.x64.debug\CoreConsole.exe \coreclr-demo\runtime\HelloWorld.exe
 cd \coreclr-demo
 nuget.exe restore packages\packages.config -Source https://www.myget.org/F/dotnet-corefx/ -PackagesDirectory packages
-copy /Y packages\Microsoft.NETCore.Runtime.CoreCLR.ConsoleHost-x64.1.0.0-beta-22819\native\win\x64\CoreConsole.exe runtime\HelloWorld.exe
 for /r packages %k in (*.dll) do echo "%k" | findstr "api-ms-win-core-synch api-ms-win-core-winrt-l1 api-ms-win-core-winrt-roparameterizediid api-ms-win-core-winrt-string api-ms-win-ro-typeresolution" && copy /Y "%k" runtime
 csc /nostdlib /noconfig /out:runtime\HelloWorld.dll /r:runtime\mscorlib.dll HelloWorld.cs
 runtime\HelloWorld.exe
